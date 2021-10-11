761f90a1d41f1b90c017c929b3bff2a51dd34810
Horolsky <horolsky89@outlook.com>
2021-10-11 19:11:24 +0300

    proto db: move walking and upd handling to Monitor

--- proto/monitor/application.py
+++ proto/monitor/application.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 import sys
-from os.path import abspath
+from os.path import isfile, abspath, getmtime, join as joinpath
+from os import walk, scandir, listdir
+from typing import List, Tuple, Callable
+
 from PyQt5.QtCore import QCoreApplication
 from pathlib import Path
 
@@ -12,21 +15,55 @@ HOME_DIR = str(Path.home())
 VIDEOS_DIR = F"{HOME_DIR}/Videos"
 
 class Monitor:
+    
+    @staticmethod
+    def walk_dir( entry: str) -> Tuple[List[str]]:
+            
+            dirs = []
+            files = []
+            
+            for root, dirnames, filenames in walk(entry, topdown=False):
+                    
+                for dname in dirnames:
+                    dirpath = joinpath(root, dname)
+                    modified = getmtime(dirpath)
+                    dirs.append((dirpath, modified))
+
+                for fname in filenames:
+                    filepath = joinpath(root, fname)
+                    modified = getmtime(filepath)
+                    files.append((filepath, modified))
+                
+            return (dirs, files)
+    @staticmethod 
+    def scan_dir(dirpath: str):
+        path = lambda entry: f"{dirpath}/{entry}"
+        return [ (path(f), getmtime(path(f)))  for f in listdir(dirpath) if isfile(path(f))]
+
+
+        
     def __init__(self) -> None:
 
-        dirs, files = WatchDog.walk_dir(VIDEOS_DIR)
+        dirs, files = Monitor.walk_dir(VIDEOS_DIR)
         paths = [VIDEOS_DIR] + [path for  path, _ in dirs] #+ [path for  path, _ in files]
         
-        db = DataBase()
+        self.db = DataBase()
         self.watchdog = WatchDog(paths, self.on_upd)
         
-        to_delete = list(set(db.get_files()).difference(set(files)))
+        to_delete = [ (path,) for path, _ in set(self.db.get_files()).difference(set(files))]
         
-        db.remove_files(to_delete)
-        db.update_files(files)
+        self.db.remove_files(to_delete)
+        self.db.update_files(files)
         
     def on_upd(self, dirpath: str) -> None:
         print(f"dir upd: {dirpath}")
+
+        files = Monitor.scan_dir(dirpath)
+        
+        to_delete = [ (path,) for path, _ in set(self.db.get_files()).difference(set(files))]
+        
+        self.db.remove_files(to_delete)
+        self.db.update_files(files)
 
 
 INIT_MSG = \
--- proto/monitor/watchdog.py
+++ proto/monitor/watchdog.py
@@ -1,30 +1,7 @@
-from os import walk
-import os.path
 from typing import List, Tuple, Callable
 from PyQt5.QtCore import QFileSystemWatcher
 
 class WatchDog:
-
-    @staticmethod
-    def walk_dir( entry: str) -> Tuple[List[str]]:
-            
-            dirs = []
-            files = []
-            
-            for root, dirnames, filenames in walk(entry, topdown=False):
-                    
-                for dname in dirnames:
-                    dirpath = os.path.join(root, dname)
-                    modified = os.path.getmtime(dirpath)
-                    dirs.append((dirpath, modified))
-
-                for fname in filenames:
-                    filepath = os.path.join(root, fname)
-                    modified = os.path.getmtime(filepath)
-                    files.append((filepath, modified))
-                
-            return (dirs, files)
-
     def __init__(self, paths: List[str], on_upd: Callable[[str], None]) -> None:
         print("monitor init")
 
