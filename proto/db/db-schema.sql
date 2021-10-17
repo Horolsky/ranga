ef5ef08afac81cdd32b4a89b2c5afb48aa93d045
Horolsky <horolsky89@outlook.com>
2021-10-17 21:11:18 +0300

    db: restructure schema by query types

--- proto/db/db-schema.sql
+++ proto/db/db-schema.sql
@@ -1,13 +1,8 @@
 PRAGMA foreign_keys = ON;
 
-CREATE TABLE IF NOT EXISTS [last_rows] (
-    id INTEGER PRIMARY KEY,
-    file_id INTEGER,
-    mkey_id INTEGER,
-    mvalue_id INTEGER,
-    mmap_rowid INTEGER
-);
-INSERT OR IGNORE INTO last_rows(id) VALUES (0);
+/* 
+DATA TABLES
+*/
 
 /* file registry */
 CREATE TABLE IF NOT EXISTS [files] (
@@ -15,10 +10,37 @@ CREATE TABLE IF NOT EXISTS [files] (
     [path] STRING NOT NULL UNIQUE,
     [parent] INTEGER, -- NULL if root entry
     [modified] REAL NOT NULL,
-    -- root entry points to itself
     [is_dir] INTEGER NOT NULL,
     FOREIGN KEY(parent) REFERENCES files(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+/* file metadata keys */
+CREATE TABLE IF NOT EXISTS [meta_keys] (
+    [id] INTEGER PRIMARY KEY,
+    [mkey] STRING UNIQUE,
+    [mtype] STRING,
+    [descr] STRING
+);
+
+/* file metadata store */
+CREATE TABLE IF NOT EXISTS [meta_values] (
+    [id] INTEGER PRIMARY KEY,
+    [mkey_id] INTEGER,
+    [mvalue] ANY, -- weak typing
+    FOREIGN KEY(mkey_id) REFERENCES meta_keys(id) ON DELETE CASCADE
+);
+
+/* file to metadata map (M:M) */
+CREATE TABLE IF NOT EXISTS [meta_map] (
+    [file_id] INTEGER,
+    [mvalue_id] INTEGER,
+    FOREIGN KEY(file_id) REFERENCES files(id) ON DELETE CASCADE,
+    FOREIGN KEY(mvalue_id) REFERENCES meta_values(id) ON DELETE CASCADE
+);
+
+/* 
+DATA INDICI
+*/
 
 CREATE INDEX IF NOT EXISTS idx_files_paths 
 ON [files] (path);
@@ -26,6 +48,74 @@ ON [files] (path);
 CREATE INDEX IF NOT EXISTS idx_files_parent 
 ON [files] (parent);
 
+CREATE UNIQUE INDEX IF NOT EXISTS idx_mkeys 
+ON [meta_keys] (mkey);
+
+CREATE INDEX IF NOT EXISTS idx_mvalues 
+ON [meta_values] (mvalue); -- TODO test performance for weak typing
+
+CREATE INDEX IF NOT EXISTS idx_mvalues_keys 
+ON [meta_values] (mkey_id);
+
+CREATE INDEX IF NOT EXISTS idx_map_files 
+ON [meta_map] (file_id);
+
+CREATE INDEX IF NOT EXISTS idx_map_values 
+ON [meta_map] (mvalue_id);
+
+/*
+UTILITY LAST_ROWS TABLE & TRIGGERS
+*/
+
+CREATE TABLE IF NOT EXISTS [last_rows] (
+    id INTEGER PRIMARY KEY,
+    file_id INTEGER,
+    mkey_id INTEGER,
+    mvalue_id INTEGER,
+    mmap_rowid INTEGER
+);
+
+INSERT OR IGNORE INTO last_rows(id) VALUES (0);
+
+CREATE TRIGGER IF NOT EXISTS [mkeys_lastrow] 
+AFTER INSERT ON [files]
+BEGIN
+    UPDATE [last_rows]
+    SET
+    file_id = NEW.id
+    WHERE id = 0;
+END;
+
+CREATE TRIGGER IF NOT EXISTS [mkeys_lastrow] 
+AFTER INSERT ON [meta_keys]
+BEGIN
+    UPDATE [last_rows]
+    SET
+    mkey_id = NEW.id
+    WHERE id = 0;
+END;
+
+CREATE TRIGGER IF NOT EXISTS [mvalues_lastrow] 
+AFTER INSERT ON [meta_values]
+BEGIN
+    UPDATE [last_rows]
+    SET
+    mvalue_id = NEW.id
+    WHERE id = 0;
+END;
+
+CREATE TRIGGER IF NOT EXISTS [mmap_lastrow] 
+AFTER INSERT ON [meta_map]
+BEGIN
+    UPDATE [last_rows]
+    SET
+    mmap_rowid = NEW.rowid
+    WHERE id = 0;
+END;
+
+/*
+DATA TRIGGERS
+*/
 
 /* put parent as path, replace with id */
 CREATE TRIGGER IF NOT EXISTS [files_find_parent] 
@@ -38,50 +128,7 @@ BEGIN
     WHERE id = NEW.id;
 END;
 
-CREATE TRIGGER IF NOT EXISTS [mkeys_lastrow] 
-AFTER INSERT ON [files]
-BEGIN
-    UPDATE [last_rows]
-    SET
-    file_id = NEW.id
-    WHERE id = 0;
-END;
-
-/* file metadata keys */
-CREATE TABLE IF NOT EXISTS [meta_keys] (
-    [id] INTEGER PRIMARY KEY,
-    [mkey] STRING UNIQUE,
-    [mtype] STRING,
-    [descr] STRING
-);
-
-CREATE UNIQUE INDEX IF NOT EXISTS idx_mkeys 
-ON [meta_keys] (mkey);
-
-CREATE TRIGGER IF NOT EXISTS [mkeys_lastrow] 
-AFTER INSERT ON [meta_keys]
-BEGIN
-    UPDATE [last_rows]
-    SET
-    mkey_id = NEW.id
-    WHERE id = 0;
-END;
-
-/* file metadata store */
-CREATE TABLE IF NOT EXISTS [meta_values] (
-    [id] INTEGER PRIMARY KEY,
-    [mkey_id] INTEGER,
-    [mvalue] ANY, -- weak typing
-    FOREIGN KEY(mkey_id) REFERENCES meta_keys(id) ON DELETE CASCADE
-);
-
-CREATE INDEX IF NOT EXISTS idx_mvalues 
-ON [meta_values] (mvalue); -- TODO test performance for weak typing
-
-CREATE INDEX IF NOT EXISTS idx_mvalues_keys 
-ON [meta_values] (mkey_id);
-
-/* ignore if record exists */
+/* ignore if record exists (no uniques) */
 CREATE TRIGGER IF NOT EXISTS [mvalues_ignore_dupes] 
 BEFORE INSERT ON [meta_values]
 WHEN 
@@ -120,32 +167,8 @@ BEGIN
     WHERE id = NEW.id;
 END;
 
-CREATE TRIGGER IF NOT EXISTS [mvalues_lastrow] 
-AFTER INSERT ON [meta_values]
-BEGIN
-    UPDATE [last_rows]
-    SET
-    mvalue_id = NEW.id
-    WHERE id = 0;
-END;
-
-/* file to metadata map (M:M) */
-CREATE TABLE IF NOT EXISTS [meta_map] (
-    [file_id] INTEGER,
-    [mvalue_id] INTEGER,
-    FOREIGN KEY(file_id) REFERENCES files(id) ON DELETE CASCADE,
-    FOREIGN KEY(mvalue_id) REFERENCES meta_values(id) ON DELETE CASCADE
-);
-
-CREATE INDEX IF NOT EXISTS idx_map_files 
-ON [meta_map] (file_id);
-
-CREATE INDEX IF NOT EXISTS idx_map_values 
-ON [meta_map] (mvalue_id);
-
-
-/* put key_id as mkey string, replace with id */
-CREATE TRIGGER IF NOT EXISTS [mmap_find_file] 
+/* put file_id as path and mvalue_id as trigger literal, replace with ids */
+CREATE TRIGGER IF NOT EXISTS [mmap_find_ids] 
 AFTER INSERT ON [meta_map]
     WHEN TYPEOF(NEW.file_id) <> 'INTEGER' AND NEW.mvalue_id = "LASTROW"
 BEGIN
@@ -156,14 +179,9 @@ BEGIN
     WHERE rowid = NEW.rowid;
 END;
 
-CREATE TRIGGER IF NOT EXISTS [mmap_lastrow] 
-AFTER INSERT ON [meta_map]
-BEGIN
-    UPDATE [last_rows]
-    SET
-    mmap_rowid = NEW.rowid
-    WHERE id = 0;
-END;
+/*
+VIEWS
+*/
 
 /* metadata + key info */
 CREATE VIEW IF NOT EXISTS meta_data AS
@@ -175,4 +193,3 @@ SELECT
 FROM
     meta_keys
     JOIN meta_values ON meta_values.mkey_id = meta_keys.id;
-
