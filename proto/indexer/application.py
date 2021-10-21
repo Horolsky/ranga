0e016f9bff16b769c8e87712a2cd14f63826254d
Horolsky <horolsky89@outlook.com>
2021-10-22 00:34:20 +0300

    proto: mv indexer cli_parser to sep file

--- proto/indexer/application.py
+++ proto/indexer/application.py
@@ -1,61 +1,28 @@
 #!/usr/bin/env python3
 
-from os import error
-from os.path import dirname, realpath 
-import argparse
 import sys
-from typing import List
-import yaml
 
-PROG = "ffpam"
-SCRIPT_DIR = dirname(realpath(__file__))
-COMMANDS_FILE = SCRIPT_DIR + "/commands.yml"
+from .cli_parser import build_argparser
 
-INFO = {
-    "prog": "ffpam",
-    "description": "local media file indexer",
-} 
+def exec_search(**kwargs):
+    print(kwargs)
 
-def search(*args, **kwargs):
-    print((args, kwargs))
+def exec_show(**kwargs):
+    print(kwargs)
 
-def show(*args, **kwargs):
-    print((args, kwargs))
+def exec_tables(**kwargs):
+    print(kwargs)
 
-def tables(*args, **kwargs):
-    print((args, kwargs))
-
-def monitor(*args, **kwargs):
-    print((args, kwargs))
+def exec_monitor(**kwargs):
+    print(kwargs)
 
 EXEC = {
-    "search": search,
-    "show": show,
-    "tables": tables,
-    "monitor": monitor
+    "search": exec_search,
+    "show": exec_show,
+    "tables": exec_tables,
+    "monitor": exec_monitor
 }
 
-def build_argparser() -> argparse.ArgumentParser:
-
-    commands_src: dict = None
-
-    with open(COMMANDS_FILE, "r") as stream:
-        try:
-            commands_src = yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            error("missing `commands.yml'")
-            print(exc)
-    parser = argparse.ArgumentParser(**INFO)
-    subparsers = parser.add_subparsers(dest='command', help='command help')
-    for cmd in commands_src:
-        descr = commands_src[cmd].get('help', '')
-        cmd_parser = subparsers.add_parser(cmd, help=descr)
-
-        for arg in commands_src[cmd].get('arguments', []):
-            names = arg.pop('names')
-            cmd_parser.add_argument(*names, **arg)
-
-    return parser
 
 def main() -> int:
     parser = build_argparser()
---
+++ proto/indexer/cli_parser.py
@@ -0,0 +1,33 @@
+import argparse
+from os import error
+import yaml
+from os.path import dirname, realpath 
+
+INFO = {
+    "prog": "ffpam",
+    "description": "local media file indexer",
+} 
+SCRIPT_DIR = dirname(realpath(__file__))
+COMMANDS_FILE = SCRIPT_DIR + "/commands.yml"
+
+def build_argparser() -> argparse.ArgumentParser:
+
+    commands_src: dict = None
+
+    with open(COMMANDS_FILE, "r") as stream:
+        try:
+            commands_src = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            error(f"missing {COMMANDS_FILE}")
+            print(exc)
+    parser = argparse.ArgumentParser(**INFO)
+    subparsers = parser.add_subparsers(dest='command', help='command help')
+    for cmd in commands_src:
+        descr = commands_src[cmd].get('help', '')
+        cmd_parser = subparsers.add_parser(cmd, help=descr)
+
+        for arg in commands_src[cmd].get('arguments', []):
+            names = arg.pop('names')
+            cmd_parser.add_argument(*names, **arg)
+
+    return parser
