50ec123924a4b5951c53d59b30c975fd19e3cdaf
Horolsky <horolsky89@outlook.com>
2021-10-22 00:59:32 +0300

    proto: add mut.excl. arg group on monitor cmd

--- proto/indexer/cli_parser.py
+++ proto/indexer/cli_parser.py
@@ -20,14 +20,15 @@ def build_argparser() -> argparse.ArgumentParser:
         except yaml.YAMLError as exc:
             error(f"missing {COMMANDS_FILE}")
             print(exc)
-    parser = argparse.ArgumentParser(**INFO)
-    subparsers = parser.add_subparsers(dest='command', help='command help')
+    main_parser = argparse.ArgumentParser(**INFO)
+    subparsers = main_parser.add_subparsers(dest='command', help='command help')
     for cmd in commands_src:
         descr = commands_src[cmd].get('help', '')
-        cmd_parser = subparsers.add_parser(cmd, help=descr)
-
+        parser = subparsers.add_parser(cmd, help=descr)
+        if cmd == 'monitor':
+            parser = parser.add_mutually_exclusive_group()
         for arg in commands_src[cmd].get('arguments', []):
             names = arg.pop('names')
-            cmd_parser.add_argument(*names, **arg)
+            parser.add_argument(*names, **arg)
 
-    return parser
+    return main_parser
