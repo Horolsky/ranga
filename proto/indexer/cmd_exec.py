c26b8ad07a544763e6560cc02c18490aad4cb9bc
Horolsky <horolsky89@outlook.com>
2021-10-25 18:22:25 +0300

    add indexer cmd executor

--- proto/indexer/cmd_exec.py
+++ proto/indexer/cmd_exec.py
@@ -1,3 +1,22 @@
+import multiprocessing
+import os 
+import subprocess
+from os.path import abspath
+# from multiprocessing.connection import Client
+
+from proto.monitor.application import main as monitor_run
+from proto.monitor.server import HOST, PORT
+from proto.db.manager import DbManager
+# from proto.monitor.qtclient import Client
+
+TIMEOUT = 1000
+# from proto.monitor.client import Client
+
+def serv_is_running():
+    import socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex((HOST, PORT)) == 0
+
 def exec_search(**kwargs):
     print(kwargs)
 
@@ -8,7 +27,48 @@ def exec_tables(**kwargs):
     print(kwargs)
 
 def exec_monitor(**kwargs):
-    print(kwargs)
+    
+    if kwargs['list']:
+        paths_to_watch = DbManager().get_root_dirs()
+        print('\n'.join(paths_to_watch))
+
+    if kwargs['add']:
+        paths = { abspath(path) for path in kwargs["add"] }
+        if serv_is_running():
+            updcmd = f'printf "add:{";".join(paths)}" | nc {HOST} {PORT} &'
+            os.system(updcmd)
+        else:
+            DbManager().add_roots(paths)
+        
+    if kwargs['remove']:
+        paths = { abspath(path) for path in kwargs["remove"] }
+        if serv_is_running():
+            updcmd = f'printf "remove:{";".join(paths)}" | nc {HOST} {PORT} &'
+            os.system(updcmd)
+        else:
+            DbManager().remove_files(paths)
+
+    if kwargs['update']:
+        if serv_is_running():
+            updcmd = f'printf "update:{";".join(kwargs["update"])}" | nc {HOST} {PORT} &'
+            os.system(updcmd)
+        else:
+            print("monitor is not launched")
+        
+    if kwargs['run']:
+        process = multiprocessing.Process(target=monitor_run, daemon=True)
+        process.start()
+        
+    if kwargs['stop']:
+        stopcmd = f'printf "stop:none" | nc {HOST} {PORT} &'
+        os.system(stopcmd)
+    if kwargs['port'] == 'get':
+        print(PORT)
+        #TODO: move port number to env
+    if kwargs['status']:
+        running = serv_is_running()
+        port = PORT
+        print(f"running:\t{running}\nport:\t\t{port}")
 
 EXEC = {
     "search": exec_search,
