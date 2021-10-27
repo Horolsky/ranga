60c5ceb43a7e6aa4e4ffbcc85f5d1b43a1d9faf1
Horolsky <horolsky89@outlook.com>
2021-10-27 23:14:23 +0300

    indexer: update monitor subcommand

--- proto/indexer/cmd_exec.py
+++ proto/indexer/cmd_exec.py
@@ -1,7 +1,10 @@
 import logging
-from os import fork, system
+from os import fork
 from os.path import abspath
+import json
 from typing import Callable
+
+from multiprocessing.connection import Client
 
 from proto.monitor.application import main as monitor_run
 from proto.monitor.server import HOST, PORT
@@ -14,70 +17,74 @@ def run_fork_proc(entry: Callable):
         return
     entry()
 
-def serv_is_running():
+def serv_is_running() -> bool:
     import socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         return s.connect_ex((HOST, PORT)) == 0
 
 def exec_search(**kwargs):
     logging.debug("indexer: search call")
-    logging.debug(str(kwargs))
+    logging.debug(f"indexer args: {str(kwargs)}")
 
 def exec_show(**kwargs):
     logging.debug("indexer: show call")
-    logging.debug(str(kwargs))
+    logging.debug(f"indexer args: {str(kwargs)}")
 
 def exec_tables(**kwargs):
     logging.debug("indexer: tables call")
-    logging.debug(str(kwargs))
+    logging.debug(f"indexer args: {str(kwargs)}")
 
 def exec_monitor(**kwargs):
     logging.debug("indexer: monitor call")
-    logging.debug(str(kwargs))
+    logging.debug(f"indexer args: {str(kwargs)}")
 
-    if kwargs['list']:
+    kwargs = {k : kwargs[k] for k in kwargs if kwargs[k] }
+    if len(kwargs) != 1:
+        raise KeyError("can execute only one subcommand at once")
+    cmd = list(kwargs.keys())[0]
+    arg = kwargs[cmd]
+    
+    if cmd == 'list':
         with DbManager() as db:
             paths_to_watch = db.get_root_dirs()
             print('\n'.join(paths_to_watch))
 
-    if kwargs['add']:
-        paths = { abspath(path) for path in kwargs["add"] }
-        if serv_is_running():
-            updcmd = f'printf "add:{";".join(paths)}" | nc {HOST} {PORT} &'
-            system(updcmd)
-        else:
-            with DbManager() as db:
-                db.add_roots(paths)
-        
-    if kwargs['remove']:
-        paths = { abspath(path) for path in kwargs["remove"] }
-        if serv_is_running():
-            updcmd = f'printf "remove:{";".join(paths)}" | nc {HOST} {PORT} &'
-            system(updcmd)
-        else:
-            with DbManager() as db:
-                db.remove_files({(path,) for path in paths})
-
-    if kwargs['update']:
-        if serv_is_running():
-            updcmd = f'printf "update:{";".join(kwargs["update"])}" | nc {HOST} {PORT} &'
-            system(updcmd)
-        else:
-            print("monitor is not launched")
-        
-    if kwargs['run']:
-        run_fork_proc(monitor_run)
-        
-    if kwargs['stop']:
-        stopcmd = f'printf "stop:none" | nc {HOST} {PORT} &'
-        system(stopcmd)
-    if kwargs['port'] == 'get':
-        print(PORT)
+    elif cmd == 'port':
+        if arg == 'get':
+            print(PORT)
         #TODO: move port number to env
-    if kwargs['status']:
+
+    elif cmd == 'status':
         running = serv_is_running()
         port = PORT
         print(f"running:\t{running}\nport:\t\t{port}")
+
+    elif cmd == 'run':
+        run_fork_proc(monitor_run)
+
+    elif cmd == 'stop':
+        conn = Client((HOST, PORT))
+        req_obj = bytes(json.dumps({ "command": "stop", "args": True }), encoding='ascii')
+        logging.debug(f"indexer req_obj: {req_obj}")
+        conn.send_bytes(req_obj)
+        conn.close()
+
+    elif cmd in ('add', 'remove', 'update'):
+        arg = list({ abspath(path) for path in arg })
+        if serv_is_running():
+            req_obj = bytes(json.dumps({ "command": cmd, "args": arg }), encoding='ascii')
+            logging.debug(f"indexer req_obj: {req_obj}")
+            conn = Client((HOST, PORT))
+            conn.send_bytes(req_obj)
+            conn.close()
+        elif cmd == 'add':
+            with DbManager() as db:
+                db.add_roots(arg)
+        elif cmd == 'remove':
+            with DbManager() as db:
+                db.remove_files({(path,) for path in arg})
+        elif cmd == 'update':
+            print("monitor is not launched")    
 
 EXEC = {
     "search": exec_search,
@@ -88,7 +95,7 @@ EXEC = {
 
 def execute_command(args: dict) -> None:
     try:
-        cmd = args['command']
+        cmd = args.pop('command')
         func = EXEC[cmd]
         func(**args)
     except: None
